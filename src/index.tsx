af6b95f2b094fc8a88af43f498b7dd57e379c7fc
Luciano Alves <luciano.mamonas98@gmail.com>
2021-07-25 21:59:47 -0300

    Added Images to component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
-const AlphaSlider = () => (
-  <div>
-    <h1>React Alpha Slider</h1>
-  </div>
-);
+import { Container, Wrapper } from "./styles";
+
+const AlphaSlider = () => {
+  const images = [
+    "https://images.unsplash.com/photo-1626375555956-1e87e7aa92b5?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1626375555783-f5fc7149e03f?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  ];
+
+  return (
+    <Container>
+      <Wrapper>
+        <img src={images[1]} alt="#" />
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default AlphaSlider;
---
+++ src/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 10px;
+  padding-top: 20px;
+`;
+
+export const Wrapper = styled.div`
+  height: 300px;
+
+  img {
+    width: 100%;
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+    border-radius: 4px;
+  }
+`;
